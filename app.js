e7aa58b6ed4cc4bc5efd3d1aae29e50bb1f651dd
tparlmer <tparlmer@gmail.com>
2022-03-31 13:53:55 -0400

    added inquirer to portfolio-generator

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template');
-
-const profileDataArgs = process.argv.slice(2);
-
-const [name, github] = profileDataArgs;
+const inquirer = require('inquirer');
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
 //  SYNTAX: fx.writeFile('arg1'-name of the file, 'arg2'-function that generates file content, 'arg3'-callback function for error handling)  
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
+// fs.writeFile('index.html', generatePage(name, github), err => {
+//     if (err) throw err;
 
-    console.log('Portfolio complete! Check out index.html to see the output!');
-})
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// })
